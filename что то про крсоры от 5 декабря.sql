17ed6c7ee828fe55cdaaf960ec1f55b45f0c2a48
Unknown <Danil.Koporushkin@urfu.me>
2017-12-05 14:15:12 +0500

    5 декабря

--- что то про крсоры от 5 декабря.sql
+++ что то про крсоры от 5 декабря.sql
@@ -20,4 +20,13 @@ declare c1 cursor for
 select * from dcur
 open c1
 declare @x1 nvarchar(50),@x2 int,@x3 int,@x4 int,@x5 money
-fetch next from c1
+fetch next from c1 into @x1,@x2,@x3,@x4,@x5
+while @@FETCH_STATUS=0
+	begin
+		if @x4=8 and @x3=1996
+			print cast(@x1 as char(20))+cast(@x2 as char(3))
+					+cast(@x3 as char(5))+cast(@x4 as char(3)) + cast(@x5 as char(15))
+		fetch next from c1 into @x1,@x2,@x3,@x4,@x5
+	end
+	close c1
+	deallocate c1
